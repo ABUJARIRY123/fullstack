1191a0d99e2f5904387828f8446256af92196350
ABUJARIRY123 <maroridhiwani@gmail.com>
2023-12-16 10:18:55 +0300

    doing good

--- server/src/main/java/com/sufyaan/server/ServerApplication.java
+++ server/src/main/java/com/sufyaan/server/ServerApplication.java
@@ -2,12 +2,21 @@ package com.sufyaan.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Welcome to Spring";
 	}
 
 }
